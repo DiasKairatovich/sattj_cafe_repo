0c22350c119a564131c0d4fb9b3d84e98088cd11
Dias Home Fedora <shaymerdenov.dias@bk.ru>
2025-03-09 17:29:58 +0500

    New changes

--- django_project/sattj_cafe/wsgi.py
+++ django_project/sattj_cafe/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sattj_cafe_django project.
+WSGI config for django_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sattj_cafe_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
